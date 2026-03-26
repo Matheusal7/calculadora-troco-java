2850d14b3dd11c50c3edf11a216616532fcaddff
Matheusal7 <152080673+Matheusal7@users.noreply.github.com>
2026-03-26 01:27:26 -0300

    trocando Double por double para usar tipos primitivos.

--- CalcularValorTroco.java
+++ CalcularValorTroco.java
@@ -7,14 +7,18 @@ public class CalcularValorTroco {
 		public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		System.out.print("Digite o valor do produto: ");
-		Double valorProduto = scanner.nextDouble();
+		double valorProduto = scanner.nextDouble();
 		
 		System.out.print("Digite quantidade passada pelo cliente: ");
-		Double valorPassadoPeloCliente = scanner.nextDouble();
+		double valorPassadoPeloCliente = scanner.nextDouble();
 		
-		Double resultado = valorPassadoPeloCliente - valorProduto;	
+		double resultado = valorPassadoPeloCliente - valorProduto;	
 		
 		System.out.println("Troco: " + resultado);
+		
+		scanner.close();
+		
+		
 			
 			
 			
